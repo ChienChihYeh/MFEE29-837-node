2ebb771115b581aaa6c016ae1f517f1be8484dfc
flybear <goodgood0331@gmail.com>
2022-11-12 14:19:38 +0800

    product_sql

--- routes/product.js
+++ routes/product.js
@@ -3,9 +3,22 @@ const router = express.Router();
 const db = require(__dirname + '/../modules/db_connect2.js');
 const upload = require(__dirname + '/../modules/upload-img')
 
-router.get('/',async(req,res)=>{
-    const [rows] = await db.query('SELECT *  FROM product ')
+router.get('/all',async (req,res)=>{
+    const [rows] = await db.query('SELECT * FROM product')
+    res.json(rows);
+})
+
+router.post('/',upload.none(),async(req,res)=>{
+    const [rows] = await db.query(`SELECT *  FROM product WHERE brand_sid=${req.body.brand} AND product_price>=${req.body.lowPrice} AND product_price<=${req.body.highPrice}`)
     res.json(rows); 
+})
+
+
+router.post('/price',upload.none(),async(req,res)=>{
+    // ${req.body.highPrice}
+    // ${req.body.lowPrice} 
+    const [rows] = await db.query(`SELECT *  FROM product WHERE product_price BETWEEN   ${req.body.lowPrice}  AND ${req.body.highPrice}  `)
+    res.json(rows);
 })
 
 module.exports = router;
