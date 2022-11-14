fa1c082a3f885ab632abc8e9663551debefc3d98
bh20511 <bh20511@gmail.com>
2022-11-14 10:29:43 +0800

    rental select add

--- routes/rental.js
+++ routes/rental.js
@@ -1,8 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require(__dirname + '/../modules/db_connect2.js');
-const upload = require(__dirname + '/../modules/upload-img')
+const db = require(__dirname + "/../modules/db_connect2.js");
+const upload = require(__dirname + "/../modules/upload-img");
 
+async function getListData(req) {
+  const sql = `SELECT * FROM rental`;
+  [rows] = await db.query(sql);
 
+  return { rows };
+}
+
+router.get("/api", async (req, res) => {
+  res.json(await getListData(req));
+});
 
 module.exports = router;
