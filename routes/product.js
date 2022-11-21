b80ebae6d0e340b370ea49f8a66f190907071593
nelson <goodgood0331@gmail.com>
2022-11-21 19:13:57 +0800

    1112zx1913

--- routes/product.js
+++ routes/product.js
@@ -20,8 +20,44 @@ router.get('/all',async (req,res)=>{
     const [rows] = await db.query('SELECT * FROM product')
     res.json(rows);
 })
+//最新商品
+router.get('/new',async (req,res)=>{
+    const [rows] = await db.query('SELECT * FROM product ORDER BY product_sid DESC')
+    res.json(rows);
+})
+//熱門商品
+router.get('/hot',async (req,res)=>{
+    const [rows] = await db.query('SELECT * FROM product ORDER BY product_sid')
+    res.json(rows);
+})
+//服飾
+router.get('/clothe',async (req,res)=>{
+    const [rows] = await db.query('SELECT * FROM product WHERE product_category_sid=2 or product_category_sid=9 or product_category_sid=10 or product_category_sid=11 or product_category_sid=12')
+    res.json(rows);
+})
+//背包
+router.get('/bag',async (req,res)=>{
+    const [rows] = await db.query('SELECT * FROM product WHERE product_category_sid=3')
+    res.json(rows);
+})
+//鞋子
+router.get('/shose',async (req,res)=>{
+    const [rows] = await db.query('SELECT * FROM product WHERE product_category_sid=4 or product_category_sid=7 or product_category_sid=8')
+    res.json(rows);
+})
+//專業用品
+router.get('/accessories',async (req,res)=>{
+    const [rows] = await db.query('SELECT * FROM product WHERE product_category_sid=5 or product_category_sid=6 or product_category_sid=15')
+    res.json(rows);
+})
+//亂數生成
 router.get('/random',async (req,res)=>{
     const [rows] = await db.query('SELECT * FROM product ORDER BY RAND() LIMIT 3')
+    res.json(rows);
+})
+
+router.get('/:prodcut_sid',async (req,res)=>{
+    const [rows] = await db.query(`SELECT * FROM product WHERE product_sid=${req.params.prodcut_sid}`)
     res.json(rows);
 })
 
