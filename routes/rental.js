6dd5316ef94bfb17f63504f9dd0804041709db83
bh20511 <bh20511@gmail.com>
2022-11-25 19:43:05 +0800

    11/25

--- routes/rental.js
+++ routes/rental.js
@@ -4,28 +4,40 @@ const db = require(__dirname + "/../modules/db_connect2.js");
 const upload = require(__dirname + "/../modules/upload-img");
 
 //商品大頁搜尋
-async function getListData(req){
+async function getListData(req) {
 
     const sql = `SELECT * FROM rental`;
     [rows] = await db.query(sql);
+    console.log(rows)
+
+    rows.map((v, i) => {
+        return (
+            v.rental_img = v.rental_img.split(',')
+        )
+    })
+    rows.map((v, i) => {
+        return (
+            v.rental_label = v.rental_label.split(',')
+        )
+    })
     
-    return {rows};
+    return rows;
 }
-router.get('/api', async (req, res)=>{
+router.get('/api', async (req, res) => {
     res.json(await getListData(req));
 });
 
 
 //商品細節內頁
-async function getDetailData(req){
-    const {rental_product_sid} = req.params
+async function getDetailData(req) {
+    const { rental_product_sid } = req.params
     const sql = `SELECT * FROM rental where rental_product_sid =${rental_product_sid}`;
     [rows] = await db.query(sql);
-    
-    return {rows};
+
+    return { rows };
 }
 
-router.get('/getDetailData/:rental_product_sid', async (req, res)=>{
+router.get('/getDetailData/:rental_product_sid', async (req, res) => {
     res.json(await getDetailData(req));
 });
 
@@ -34,17 +46,17 @@ router.get('/getDetailData/:rental_product_sid', async (req, res)=>{
 
 
 //商品名稱模糊搜尋
-async function getSearchData(req){
-    const{search} = req.query
+async function getSearchData(req) {
+    const { search } = req.query
     const sql = `SELECT * FROM rental where rental_product_name LIKE "%${search}%"`;
     [rows] = await db.query(sql);
     console.log(sql);
-    return {rows};
+    return { rows };
 }
 
-router.get('/getSearchData', async (req, res)=>{
+router.get('/getSearchData', async (req, res) => {
     res.json(await getSearchData(req));
-    
+
 });
 module.exports = router;
 
