87a9ebc57dd3f9bb64dbd2eb753c2a9c5d356f62
YI-RU <game665987@gmail.com>
2022-11-27 04:29:20 +0800

    linepay

--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,21 @@
+const axios = require("axios");
+const { HmacSHA256 } = require("crypto-js");
+const Base64 = require("crypto-js/enc-base64");
 const express = require("express");
 const router = express.Router();
 const db = require(__dirname + "/../modules/db_connect2.js");
 const upload = require(__dirname + "/../modules/upload-img.js");
-
+require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
+const {
+  LINEPAY_CHANNEL_ID,
+  LINEPAY_CHANNEL_SECRET_KEY,
+  LINEPAY_VERSION,
+  LINEPAY_SITE,
+  LINEPAY_RETURN_HOST,
+  LINEPAY_RETURN_CONFIRM_URL,
+  LINEPAY_RETURN_CANCEL_URL,
+} = process.env;
 router.get("/api", async (req, res) => {
   //母訂單
   const momOrder =
@@ -32,6 +45,49 @@ router.get("/api", async (req, res) => {
     renRows: renRows,
     camRows: camRows,
   });
+});
+
+router.post("/createOrder", async (req, res) => {
+  const { orderID } = req.params;
+  const order = req.body;
+  try {
+    //要送出去的東西
+    const linePayBody = {
+      ...order,
+      //成功的頁面跟取消的頁面
+      redirectUrls: {
+        confirmUrl: `${LINEPAY_RETURN_HOST}/${LINEPAY_RETURN_CONFIRM_URL}`,
+        cancelUrl: `${LINEPAY_RETURN_HOST}/${LINEPAY_RETURN_CANCEL_URL}`,
+      },
+    };
+    const jsonStr = JSON.stringify(linePayBody);
+    const uri = "/payments/request";
+    const nonce = uuidv4();
+    const string = `${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${jsonStr}${nonce}`;
+    //製作簽章
+    const signature = Base64.stringify(
+      HmacSHA256(string, LINEPAY_CHANNEL_SECRET_KEY)
+    );
+    const headers = {
+      "Content-Type": "application/json",
+      "X-LINE-ChannelId": LINEPAY_CHANNEL_ID,
+      "X-LINE-Authorization-Nonce	": nonce,
+      "X-LINE-Authorization	": signature,
+    };
+
+    const url = `${LINEPAY_SITE}/${LINEPAY_VERSION}${uri}`;
+    const linePayRes = await axios.post(url, linePayBody, { headers });
+    console.log(linePayRes);
+
+    res.json(linePayRes?.data);
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
+});
+
+router.get("/test", (req, res) => {
+  res.json(uuidv4());
 });
 
 module.exports = router;
