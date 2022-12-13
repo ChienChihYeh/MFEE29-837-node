15dd8e53977095b7372ca418d8e8d0c8b58330a8
bh20511 <bh20511@gmail.com>
2022-12-13 10:47:08 +0800

    1213

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const sessionStore = new MysqlStore({}, db);
 const cors = require("cors");
 const myParser = require("body-parser");
 
+//line測試
+const line = require("@line/bot-sdk");
+
 //top-Level middleware
 //跨網域 白名單
 const corsOptions = {
@@ -52,6 +55,586 @@ io.on("connection", (socket) => {
     socket.emit("Message", message);
   });
 });
+
+//這邊是處理 一開始的callback line--bot測試
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret,
+};
+const client = new line.Client(config);
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+async function handleEvent(event) {
+  if (event.message.text === "1") {
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: "是1",
+    });
+  }
+  if (event.message.text === "大安店") {
+    return client.replyMessage(event.replyToken, {
+      type: "location",
+      title: "大安店地址",
+      address: "大安區資展路305號",
+      latitude: 25.03411859476791,
+      longitude: 121.5434656486318,
+    });
+  }
+  if (event.message.text === "八德店") {
+    return client.replyMessage(event.replyToken, {
+      type: "location",
+      title: "八德店地址",
+      address: "桃園市八德區女帝路",
+      latitude: 24.962244,
+      longitude: 121.29867,
+    });
+  }
+
+  // if (event.message.text === "測試1") {
+  //   return client.replyMessage(event.replyToken, [
+  //     {
+  //       type: "sticker",
+  //       packageId: "1",
+  //       stickerId: "1",
+  //     },
+  //     {
+  //       type: "image",
+  //       originalContentUrl:
+  //         "https://developers.line.biz/media/messaging-api/messages/image-full-04fbba55.png",
+  //       previewImageUrl:
+  //         "https://developers.line.biz/media/messaging-api/messages/image-167efb33.png",
+  //     },
+  //     {
+  //       type: "video",
+  //       originalContentUrl:
+  //         "https://www.sample-videos.com/video123/mp4/240/big_buck_bunny_240p_1mb.mp4",
+  //       previewImageUrl:
+  //         "https://www.sample-videos.com/img/Sample-jpg-image-50kb.jpg",
+  //     },
+  //     {
+  //       type: "audio",
+  //       originalContentUrl:
+  //         "https://www.sample-videos.com/audio/mp3/crowd-cheering.mp3",
+  //       duration: "27000",
+  //     },
+  //     {
+  //       type: "location",
+  //       title: "my location",
+  //       address: "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+  //       latitude: 35.65910807942215,
+  //       longitude: 139.70372892916203,
+  //     },
+  //   ]);
+  // }
+  // if (event.message.text === "測試2") {
+  //   return client.replyMessage(event.replyToken, [
+  //     {
+  //       type: "imagemap",
+  //       baseUrl:
+  //         "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/rich",
+  //       altText: "Imagemap alt text",
+  //       baseSize: { width: 1040, height: 1040 },
+  //       actions: [
+  //         {
+  //           area: { x: 0, y: 0, width: 520, height: 520 },
+  //           type: "uri",
+  //           linkUri: "https://store.line.me/family/manga/en",
+  //         },
+  //         {
+  //           area: { x: 520, y: 0, width: 520, height: 520 },
+  //           type: "uri",
+  //           linkUri: "https://store.line.me/family/music/en",
+  //         },
+  //         {
+  //           area: { x: 0, y: 520, width: 520, height: 520 },
+  //           type: "uri",
+  //           linkUri: "https://store.line.me/family/play/en",
+  //         },
+  //         {
+  //           area: { x: 520, y: 520, width: 520, height: 520 },
+  //           type: "message",
+  //           text: "URANAI!",
+  //         },
+  //       ],
+  //       video: {
+  //         originalContentUrl:
+  //           "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/video.mp4",
+  //         previewImageUrl:
+  //           "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/preview.jpg",
+  //         area: {
+  //           x: 280,
+  //           y: 385,
+  //           width: 480,
+  //           height: 270,
+  //         },
+  //         externalLink: {
+  //           linkUri: "https://line.me",
+  //           label: "LINE",
+  //         },
+  //       },
+  //     },
+  //     {
+  //       type: "template",
+  //       altText: "Buttons alt text",
+  //       template: {
+  //         type: "buttons",
+  //         thumbnailImageUrl:
+  //           "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/buttons/1040.jpg",
+  //         title: "My button sample",
+  //         text: "Hello, my button",
+  //         actions: [
+  //           { label: "Go to line.me", type: "uri", uri: "https://line.me" },
+  //           { label: "Say hello1", type: "postback", data: "hello こんにちは" },
+  //           {
+  //             label: "言 hello2",
+  //             type: "postback",
+  //             data: "hello こんにちは",
+  //             text: "hello こんにちは",
+  //           },
+  //           { label: "Say message", type: "message", text: "Rice=米" },
+  //         ],
+  //       },
+  //     },
+  //     {
+  //       type: "flex",
+  //       altText: "this is a flex message",
+  //       contents: {
+  //         type: "bubble",
+  //         body: {
+  //           type: "box",
+  //           layout: "vertical",
+  //           contents: [
+  //             {
+  //               type: "text",
+  //               text: "hello",
+  //             },
+  //             {
+  //               type: "text",
+  //               text: "world",
+  //             },
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   ]);
+  // }
+  if (event.message.text === "測試1") {
+    return client.replyMessage(event.replyToken, [
+      {
+        type: "sticker",
+        packageId: "1",
+        stickerId: "1",
+      },
+      {
+        type: "image",
+        originalContentUrl:
+          "https://developers.line.biz/media/messaging-api/messages/image-full-04fbba55.png",
+        previewImageUrl:
+          "https://developers.line.biz/media/messaging-api/messages/image-167efb33.png",
+      },
+      {
+        type: "video",
+        originalContentUrl:
+          "https://www.sample-videos.com/video123/mp4/240/big_buck_bunny_240p_1mb.mp4",
+        previewImageUrl:
+          "https://www.sample-videos.com/img/Sample-jpg-image-50kb.jpg",
+      },
+      {
+        type: "audio",
+        originalContentUrl:
+          "https://www.sample-videos.com/audio/mp3/crowd-cheering.mp3",
+        duration: "27000",
+      },
+      {
+        type: "location",
+        title: "my location",
+        address: "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+        latitude: 35.65910807942215,
+        longitude: 139.70372892916203,
+      },
+    ]);
+  }
+  // if (event.message.text === "測試2") {
+  //   return client.replyMessage(
+  //     event.replyToken,
+  //     {
+  //       type: "imagemap",
+  //       baseUrl:
+  //         "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/rich",
+  //       altText: "Imagemap alt text",
+  //       baseSize: { width: 1040, height: 1040 },
+  //       actions: [
+  //         {
+  //           area: { x: 0, y: 0, width: 520, height: 520 },
+  //           type: "uri",
+  //           linkUri: "https://store.line.me/family/manga/en",
+  //         },
+  //         {
+  //           area: { x: 520, y: 0, width: 520, height: 520 },
+  //           type: "uri",
+  //           linkUri: "https://store.line.me/family/music/en",
+  //         },
+  //         {
+  //           area: { x: 0, y: 520, width: 520, height: 520 },
+  //           type: "uri",
+  //           linkUri: "https://store.line.me/family/play/en",
+  //         },
+  //         {
+  //           area: { x: 520, y: 520, width: 520, height: 520 },
+  //           type: "message",
+  //           text: "URANAI!",
+  //         },
+  //       ],
+  //       // video: {
+  //       //   originalContentUrl:
+  //       //     "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/video.mp4",
+  //       //   previewImageUrl:
+  //       //     "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/imagemap/preview.jpg",
+  //       //   area: {
+  //       //     x: 280,
+  //       //     y: 385,
+  //       //     width: 480,
+  //       //     height: 270,
+  //       //   },
+  //       //   externalLink: {
+  //       //     linkUri: "https://line.me",
+  //       //     label: "LINE",
+  //       //   },
+  //       // },
+  //     },
+  //     {
+  //       type: "template",
+  //       altText: "Buttons alt text",
+  //       template: {
+  //         type: "buttons",
+  //         thumbnailImageUrl:
+  //           "https://github.com/line/line-bot-sdk-nodejs/raw/master/examples/kitchensink/static/buttons/1040.jpg",
+  //         title: "My button sample",
+  //         text: "Hello, my button",
+  //         actions: [
+  //           { label: "Go to line.me", type: "uri", uri: "https://line.me" },
+  //           { label: "Say hello1", type: "postback", data: "hello こんにちは" },
+  //           {
+  //             label: "言 hello2",
+  //             type: "postback",
+  //             data: "hello こんにちは",
+  //             text: "hello こんにちは",
+  //           },
+  //           { label: "Say message", type: "message", text: "Rice=米" },
+  //         ],
+  //       },
+  //     },
+  //     {
+  //       type: "flex",
+  //       altText: "this is a flex message",
+  //       contents: {
+  //         type: "bubble",
+  //         body: {
+  //           type: "box",
+  //           layout: "vertical",
+  //           contents: [
+  //             {
+  //               type: "text",
+  //               text: "hello",
+  //             },
+  //             {
+  //               type: "text",
+  //               text: "world",
+  //             },
+  //           ],
+  //         },
+  //       },
+  //     }
+  //   );
+  // }
+
+  // if (event.message.text === "嘗試一下") {
+  //   return client.replyMessage(event.replyToken, [
+  //     {
+  //       type: "template",
+  //       altText: "Buttons alt text",
+  //       template: {
+  //         type: "buttons",
+  //         thumbnailImageUrl:
+  //           process.env.ngrok + "/%E5%A4%A7%E5%AE%89%E5%BA%97.jpeg",
+  //         title: "大安店",
+  //         text: "歡迎光臨大安店",
+  //         actions: [
+  //           { label: "查詢地址", type: "uri", uri: "https://line.me" },
+  //           // { label: "店點圖二", type: "postback", data: "hello こんにちは" },
+  //           { label: "說話", type: "message", text: "我要去大安店" },
+  //         ],
+  //       },
+  //     },
+  //     {
+  //       type: "template",
+  //       altText: "Buttons alt text",
+  //       template: {
+  //         type: "buttons",
+  //         thumbnailImageUrl:
+  //           process.env.ngrok + "/%E5%85%AB%E5%BE%B7%E5%BA%97.jpeg",
+  //         title: "八德店",
+  //         text: "歡迎光臨八德店",
+  //         actions: [
+  //           { label: "查詢地址", type: "uri", uri: "https://line.me" },
+  //           // { label: "店點圖二", type: "postback", data: "hello こんにちは" },
+  //           { label: "說話", type: "message", text: "我要去大安店" },
+  //         ],
+  //       },
+  //     },
+  //     {
+  //       type: "template",
+  //       altText: "Buttons alt text",
+  //       template: {
+  //         type: "buttons",
+  //         thumbnailImageUrl:
+  //           process.env.ngrok + "/%E8%87%BA%E4%B8%AD%E5%BA%97.jpeg",
+  //         title: "台中店",
+  //         text: "歡迎光臨台中店",
+  //         actions: [
+  //           { label: "查詢地址", type: "uri", uri: "https://line.me" },
+  //           // { label: "店點圖二", type: "postback", data: "hello こんにちは" },
+  //           { label: "說話", type: "message", text: "我要去大安店" },
+  //         ],
+  //       },
+  //     },
+  //     {
+  //       type: "template",
+  //       altText: "Buttons alt text",
+  //       template: {
+  //         type: "buttons",
+  //         thumbnailImageUrl:
+  //           process.env.ngrok + "/%E9%AB%98%E9%9B%84%E5%BA%97.jpeg",
+  //         title: "高雄店",
+  //         text: "歡迎光臨高雄店",
+  //         actions: [
+  //           { label: "查詢地址", type: "uri", uri: "https://line.me" },
+  //           // { label: "店點圖二", type: "postback", data: "hello こんにちは" },
+  //           { label: "說話", type: "message", text: "我要去大安店" },
+  //         ],
+  //       },
+  //     },
+  //     {
+  //       type: "template",
+  //       altText: "Buttons alt text",
+  //       template: {
+  //         type: "buttons",
+  //         thumbnailImageUrl:
+  //           process.env.ngrok + "/%E8%8A%B1%E8%93%AE%E5%BA%97.jpeg",
+  //         title: "花蓮店",
+  //         text: "歡迎光臨花蓮店",
+  //         actions: [
+  //           { label: "查詢地址", type: "uri", uri: "https://line.me" },
+  //           // { label: "店點圖二", type: "postback", data: "hello こんにちは" },
+  //           { label: "說話", type: "message", text: "我要去大安店" },
+  //         ],
+  //       },
+  //     },
+  //   ]);
+  // }
+
+  if (event.message.text === "店點介紹") {
+    return client.replyMessage(event.replyToken, [
+      {
+        type: "template",
+        altText: "this is a carousel template",
+        template: {
+          type: "carousel",
+          columns: [
+            {
+              thumbnailImageUrl:
+                process.env.ngrok + "/%E5%A4%A7%E5%AE%89%E5%BA%97.jpeg",
+              imageBackgroundColor: "#FFFFFF",
+              title: "大安店",
+              text: "大安捷運站附近",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [{ label: "店點地址", type: "message", text: "大安店" }],
+            },
+            {
+              thumbnailImageUrl:
+                process.env.ngrok + "/%E5%85%AB%E5%BE%B7%E5%BA%97.jpeg",
+              imageBackgroundColor: "#000000",
+              title: "八德店",
+              text: "八德女帝家附近",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [{ label: "店點地址", type: "message", text: "八德店" }],
+            },
+            {
+              thumbnailImageUrl:
+                process.env.ngrok + "/%E8%87%BA%E4%B8%AD%E5%BA%97.jpeg",
+              imageBackgroundColor: "#000000",
+              title: "臺中店",
+              text: "臺中火車站旁",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [{ label: "店點地址", type: "message", text: "臺中店" }],
+            },
+            {
+              thumbnailImageUrl:
+                process.env.ngrok + "/%E9%AB%98%E9%9B%84%E5%BA%97.jpeg",
+              imageBackgroundColor: "#000000",
+              title: "高雄店",
+              text: "近高雄85大夏",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [{ label: "店點地址", type: "message", text: "高雄店" }],
+            },
+            {
+              thumbnailImageUrl:
+                process.env.ngrok + "/%E8%8A%B1%E8%93%AE%E5%BA%97.jpeg",
+              imageBackgroundColor: "#000000",
+              title: "花蓮店",
+              text: "花蓮東華大學旁邊",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [{ label: "店點地址", type: "message", text: "花蓮店" }],
+            },
+          ],
+          imageAspectRatio: "rectangle",
+          imageSize: "cover",
+        },
+      },
+    ]);
+  }
+  if (event.message.text === "導遊介紹" || event.message.text === "組員介紹") {
+    return client.replyMessage(event.replyToken, [
+      {
+        type: "template",
+        altText: "this is a carousel template",
+        template: {
+          type: "carousel",
+          columns: [
+            {
+              thumbnailImageUrl: process.env.ngrok + "/A.jpg",
+              imageBackgroundColor: "#FFFFFF",
+              title: "大奶維維導遊",
+              text: "微糖少冰",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [
+                // { label: "前往店點介紹", type: "uri", uri: "http://localhost:3000/store" },
+                {
+                  label: "鎧維",
+                  type: "message",
+                  text: "本組努力又積極的領導者",
+                },
+              ],
+            },
+            {
+              thumbnailImageUrl: process.env.ngrok + "/B.jpg",
+              imageBackgroundColor: "#000000",
+              title: "恩7導遊",
+              text: "體重變仙女中",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [
+                {
+                  label: "恩齊",
+                  type: "message",
+                  text: "人生慢慢起飛的好女孩",
+                },
+              ],
+            },
+            {
+              thumbnailImageUrl: process.env.ngrok + "/F.jpg",
+              imageBackgroundColor: "#000000",
+              title: "趴趴導遊",
+              text: "本組智商代表",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [
+                { label: "潛之", type: "message", text: "本組智商擔當" },
+              ],
+            },
+            {
+              thumbnailImageUrl: process.env.ngrok + "/D.jpg",
+              imageBackgroundColor: "#000000",
+              title: "程式瘋狗導遊",
+              text: "半夜都在打code",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [
+                {
+                  label: "錒鑐",
+                  type: "message",
+                  text: "認真投入code的辣個男人",
+                },
+              ],
+            },
+            {
+              thumbnailImageUrl: process.env.ngrok + "/E.jpg",
+              imageBackgroundColor: "#000000",
+              title: "ZX導遊",
+              text: "還在皮",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "http://localhost:3000/store",
+              },
+              actions: [
+                {
+                  label: "力誠",
+                  type: "message",
+                  text: "皮卻對code有熱忱的boy",
+                },
+              ],
+            },
+            {
+              thumbnailImageUrl: process.env.ngrok + "/C.jpg",
+              imageBackgroundColor: "#000000",
+              title: "女帝導遊",
+              text: "不服來戰",
+              defaultAction: {
+                type: "uri",
+                label: "View detail",
+                uri: "https://www.cakeresume.com/bh20511",
+              },
+              actions: [
+                { label: "柏宏", type: "message", text: "只有顏值的花瓶" },
+              ],
+            },
+          ],
+          imageAspectRatio: "rectangle",
+          imageSize: "cover",
+        },
+      },
+    ]);
+  }
+}
 
 //top-Level middleware
 //解析urlencoded,json放到req.body裡
